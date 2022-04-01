23705f5bc3daa231b737885837f3eeb76bd65792
mellabanca <91677889+mellabanca@users.noreply.github.com>
2022-04-01 14:08:15 -0300

    01/04/2022

--- sketch.js
+++ sketch.js
@@ -6,11 +6,22 @@ const Constraint = Matter.Constraint;
 const Body = Matter.Body;
 const Composites = Matter.Composites;
 const Composite = Matter.Composite;
-
+var linguica;
+var natura;
 let engine;
 let world;
-
+var grandechurrasco; 
 var ground;
+var academia;
+var nutricao;
+var alfredo;
+
+function preload(){
+  academia = loadImage("background.png");
+  nutricao = loadImage("melon.png");
+  alfredo = loadImage("Rabbit-01.png");
+
+}
 
 function setup() 
 {
@@ -19,8 +30,19 @@ function setup()
   world = engine.world;
  
   ground = new Ground(200, 690, 600, 20);
+linguica=new Rope(6,{x:245,y:30});
+
+var options={
+  density:0.001
+}
+natura=Bodies.circle(300,300,15,options);
+Matter.Composite.add(linguica.body,natura);
+
+grandechurrasco=new Grandechurrasco(linguica,natura)
+
 
   rectMode(CENTER);
+  imageMode(CENTER);
   ellipseMode(RADIUS);
   textSize(50)
 }
@@ -28,9 +50,15 @@ function setup()
 function draw() 
 {
   background(51);
+  image(academia, width/2, height/2, 500, 700);
+
   Engine.update(engine);
-   
+  image(nutricao, natura.position.x,natura.position.y,60,60);
+  
+
+
   ground.show();
+  linguica.show();
 }
 
 
